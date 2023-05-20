63cbd398466783d22957703f6a66ba380a4c1675
ramses237 <ramsesndame34@gmail.com>
2023-05-20 11:39:10 +0100

    feat:material icons and card feature
    design feature card home and add material icons to display all icons used in the application
    Reveiw_by: Victor
    Refs : #10

--- src/views/pages/home/index.vue
+++ src/views/pages/home/index.vue
@@ -4,12 +4,16 @@
             <v-row>
                 <v-col v-for="(item, index) in homeFeauture" :key="index" cols="6" >
                     <div  class="feature-card-content">
-                        <div class="feature-icon-content">
-
+                        <div class="feature-icon-content d-flex justify-center align-center">
+                            <v-icon color="white" :icon="item.icon"></v-icon>
+                            <v-icon icon="mdi-vuetify"></v-icon>
                         </div>
                         <div class="feature-descritpion-content">
-                            
+
                         </div>
+                        <v-btn position="absolute" style="right: 2rem;bottom:-1rem" >
+                            {{ $t('translate_key_start') }}
+                        </v-btn>
     
                     </div>
                 </v-col>
@@ -26,7 +30,7 @@ export default defineComponent({
         return {
             homeFeauture: [
                 {
-                    icon: '',
+                    icon: 'mdi-plus',
                     name: 'translate_key_new_project'
                 },
                 {
