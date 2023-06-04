8fdf1dcc65b2b3e75c8bf62266f100d44df03cb0
Ramses ndame <64875585+ramsesndame237@users.noreply.github.com>
2023-06-04 19:00:10 +0100

    Fix: Create a component to manage logic and design of featureCard i create a part component to manage the featureCard view to have more best manipulation Reviewed-by: Victor Refs:#16

--- src/views/pages/home/index.vue
+++ src/views/pages/home/index.vue
@@ -1,52 +1,40 @@
 <template>
-    <div class="home-page-container">
-      <v-container  >
-        <v-row>
-          <v-col v-for="(item, index) in homeFeauture" :key="index" cols="12" md="6" lg="6"  sm="12"  >
-            <div  class="feature-card-content">
-              <div class="feature-icon-content d-flex justify-center align-center flex-column">
-                <v-icon size="45" color="white" :icon="item.icon"></v-icon>
-                <span class="text-body-1 font-weight-light text-white " >
-                                {{ $t(item.name) }}
-                </span>
-              </div>
-              <div class="feature-descritpion-content">
-              <p>
-                {{ $t(item.description) }}
-              </p>
-              </div>
-              <v-btn v-if="!isMobile" position="absolute" style="right: 2rem;bottom:-1rem" >
-                {{ $t('translate_key_start') }}
-              </v-btn>
-
-            </div>
-          </v-col>
-        </v-row>
-      </v-container>
-    </div>
+  <div class="home-page-container">
+    <v-container>
+      <v-row>
+        <v-col v-for="(item, index) in homeFeauture" :key="index" cols="12" md="6" lg="6" sm="12">
+          <FeatureCard :feature-data="item"  :is-mobile = 'isMobile' />
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue';
+import {defineComponent} from 'vue';
 import {FeatureData} from "@/views/pages/home/modules/features";
 import GetUsersDeviceInfo from "@/helpers/getUsersDeviceInfo";
+import FeatureCard from '@/views/widgets/FeatureCard.vue'
 
 export default defineComponent({
 
-    name: "home-page",
-    data: () => {
-        return {
-            homeFeauture:FeatureData,
-          isMobile:false
-        }
-    },
+  name: "home-page",
+  data: () => {
+    return {
+      homeFeauture: FeatureData,
+      isMobile: false
+    }
+  },
+  components:{
+    FeatureCard
+  },
   created() {
-      console.log("this si the created")
+    console.log("this si the created")
     const DeviceInfo = new GetUsersDeviceInfo()
     this.isMobile = DeviceInfo.getDeviceRatio().isMobile
     console.log(DeviceInfo.getDeviceRatio())
   },
   mounted() {
-      console.log("failded suites", this.homeFeauture)
+    console.log("failded suites", this.homeFeauture)
   }
 })
 </script>
---
+++ src/views/widgets/FeatureCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="feature-card-content my-4">
+    <div class="feature-icon-content d-flex justify-center align-center flex-column">
+      <v-icon size="45" color="white" :icon="featureData.icon"></v-icon>
+      <span class="text-body-1 font-weight-light text-white ">
+                                {{ $t(featureData.name) }}
+                </span>
+    </div>
+    <div class="feature-descritpion-content">
+      <p>
+        {{ $t(featureData.description) }}
+      </p>
+    </div>
+    <v-btn v-if="!isMobile" position="absolute" style="right: 2rem;bottom:-1rem">
+      {{ $t('translate_key_start') }}
+    </v-btn>
+
+  </div>
+</template>
+
+<script>
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: 'FeatureCard',
+
+  props: {
+    featureData: {
+      required: true
+    },
+    isMobile: {
+      type: Boolean
+    }
+  },
+  mounted() {
+    console.log( "featureCard element",this.featureData)
+  }
+})
+</script>
